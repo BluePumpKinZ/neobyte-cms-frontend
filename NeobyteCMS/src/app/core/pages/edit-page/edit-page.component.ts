c0770f9d4854e1656fab14373b92c7ecbfb58d13
Maxim Derboven <maxim.derboven@student.kdg.be>
2023-03-07 12:37:46 +0100

    Added WYSIWYG

--- NeobyteCMS/src/app/core/pages/edit-page/edit-page.component.ts
+++ NeobyteCMS/src/app/core/pages/edit-page/edit-page.component.ts
@@ -2,6 +2,10 @@ import {AfterViewInit, Component, ComponentRef, ElementRef, Inject, OnInit, View
 import {ActivatedRoute} from "@angular/router";
 import {IframeHelperService} from "../../services/iframe-helper.service";
 import {PageService} from "../../services/page.service";
+import tinymce from "tinymce";
+import {MessageService} from "../../services/message.service";
+
+// import { Editor, EditorSettings } from 'tinymce';
 
 @Component({
   selector: 'app-edit-page',
@@ -12,28 +16,117 @@ export class EditPageComponent implements AfterViewInit, OnInit {
   @ViewChild('content_edit') iframe!: ElementRef<HTMLIFrameElement>;
   siteId: string | undefined;
   pageId: string | undefined;
+
+  // settings: EditorSettings = {}
   constructor(
     private _route: ActivatedRoute,
+    private _messageService: MessageService,
     private _iframeHelper: IframeHelperService,
     private _pageService: PageService,
-  ) { }
-
+  ) {
+  }
 
 
   ngAfterViewInit(): void {
+    this.setupEditor();
+    //get iframe and add other _iframeHelper options on it
+    // this._iframeHelper.disableInteractions(this.iframe.nativeElement);
+    // this._iframeHelper.simulateClicks(this.iframe.nativeElement, window.parent.document);
   }
 
   get_doctype(doctype: DocumentType): string {
     return '<!DOCTYPE ' +
       doctype.name +
-      (doctype.publicId?' PUBLIC "' + doctype.publicId + '"':'') +
-      (doctype.systemId?' "' + doctype.systemId + '"':'') + '>';
+      (doctype.publicId ? ' PUBLIC "' + doctype.publicId + '"' : '') +
+      (doctype.systemId ? ' "' + doctype.systemId + '"' : '') + '>';
+  }
+
+  setupEditor() {
+    var dfreeHeaderConfig = {
+      base_url: '/tinymce', // Root for resources
+      suffix: '.min',        // Suffix to use when loading resources
+      selector: '.dfree-header',
+      menubar: false,
+      inline: true,
+      toolbar: false,
+      plugins: ['quickbars'],
+      quickbars_insert_toolbar: 'undo redo',
+      quickbars_selection_toolbar: 'italic underline',
+      toolbar_persist: true,
+    };
+
+    var dfreeBodyConfig = {
+      toolbar_persist: true,
+      base_url: '/tinymce', // Root for resources
+      suffix: '.min',        // Suffix to use when loading resources
+      selector: '.dfree-body',
+      menubar: false,
+      inline: true,
+      plugins: [
+        'autolink',
+        'codesample',
+        'link',
+        'lists',
+        'media',
+        'powerpaste',
+        'table',
+        'image',
+        'quickbars',
+        'codesample',
+        'help'
+      ],
+      toolbar: false,
+      quickbars_insert_toolbar: 'quicktable image media codesample',
+      quickbars_selection_toolbar: 'bold italic underline | formatselect | blockquote quicklink',
+      contextmenu: 'undo redo | inserttable | cell row column deletetable | help',
+      powerpaste_word_import: 'clean',
+      powerpaste_html_import: 'clean',
+    };
+
+    tinymce.init(dfreeHeaderConfig);
+    tinymce.init(dfreeBodyConfig);
+
+
+    // tinymce.init({
+    //   selector: 'div.tinymce',
+    //   plugins: [ 'quickbars' ],
+    //   toolbar: false,
+    //   menubar: false,
+    //   inline: true
+    // });
+    // tinymce.init({
+    //   base_url: '/tinymce', // Root for resources
+    //   suffix: '.min',        // Suffix to use when loading resources
+    //   selector: "",
+    //   plugins: "",
+    //   toolbar: "",
+    //   menubar: false,
+    //
+    //   setup: function (editor) {
+    //     editor.ui.registry.addContextToolbar('textalignment', {
+    //       predicate: function (node) {
+    //         return editor.dom.getParent(node, '.myclass') !== null;
+    //       },
+    //       items: 'alignleft aligncenter alignright',
+    //       position: 'selection',
+    //       scope: 'node'
+    //     });
+    //   }
+    // });
   }
 
   ngOnInit(): void {
     this.siteId = this._route.snapshot.paramMap.get('siteId')!;
     this.pageId = this._route.snapshot.paramMap.get('pageId')!;
-    this._iframeHelper.get(`websites/${this.siteId}/pages/${this.pageId}/render`).subscribe(blob =>this.iframe.nativeElement.src = blob);
+    this._iframeHelper.get(`websites/${this.siteId}/pages/${this.pageId}/render`).subscribe(blob => {
+      this.iframe.nativeElement.src = blob;
+      //wait until iframe is loaded
+      this.iframe.nativeElement.onload = () => {
+        this._iframeHelper.disableInteractions(this.iframe.nativeElement);
+        //this._iframeHelper.simulateClicks(this.iframe.nativeElement, this.iframe.nativeElement.contentWindow!.document);
+        this.loadSite();
+      }
+    });
   }
 
   onPublishSite() {
@@ -44,6 +137,49 @@ export class EditPageComponent implements AfterViewInit, OnInit {
       () => {
         //this._messageService.add({type: 'success', title: 'Page', description: 'Page updated'});
       });
+  }
+
+  getDuplicateIds(doc: Document) {
+    const elements = [...doc.querySelectorAll('[id]')];
+    const ids: string[] = [];
+    const dups: Element[] = [];
+    elements.map(el => (ids.includes(el.id) ? dups.push(el) : ids.push(el.id)));
+    return dups;
+  }
+
+  private loadSite() {
+    // Blur anything that was autofocused
+    if (this.iframe.nativeElement.contentWindow!.document.activeElement) {
+      (this.iframe.nativeElement.contentWindow!.document.activeElement as HTMLElement).blur();
+    }
+
+    // Show no regions notice
+    if (![...this.iframe.nativeElement.contentWindow!.document.querySelectorAll('.cms-editable')].length) {
+      //show message that there are no regions
+      this._messageService.add({type: 'danger', title: 'No regions', description: 'There are no regions to edit'});
+    }
+
+    // Check for duplicate ids
+    this.getDuplicateIds(this.iframe.nativeElement.contentWindow!.document).map(element => {
+      if (element.classList.contains('cms-editable')) {
+        this._messageService.add({type: 'danger', title: 'Duplicate ID', description: 'Duplicate ID found'});
+      }
+    });
+
+    // Inject TinyMCE
+    const script = this.iframe.nativeElement.contentWindow!.document.createElement('script');
+    script.src = `${location.protocol}//${location.host}/tinymce/tinymce.min.js`;
+    script.onload = () => {
+      this.setupEditor();
+      this._messageService.add({type: 'success', title: 'Editor loaded', description: 'Editor (TinyMCE) loaded successfully.'});
+    }
+
+    // If TinyMCE fails to load
+    script.onerror = (error) => {
+      this._messageService.add({type: 'danger', title: 'Editor failed', description: 'Editor (TinyMCE) failed to load. Please try again later.'});
+    };
+
+    this.iframe.nativeElement.contentWindow!.document.body.appendChild(script);
   }
 }
 
--- NeobyteCMS/src/app/core/services/iframe-helper.service.ts
+++ NeobyteCMS/src/app/core/services/iframe-helper.service.ts
@@ -10,6 +10,55 @@ export class IframeHelperService {
   constructor(private http: HttpClient) {
   }
 
+  disableInteractions(iframe: HTMLIFrameElement) {
+    const doc = iframe.contentWindow!.document;
+
+    // Prevent clicks on links and image maps
+    [...doc.querySelectorAll('a, area')].map(element => {
+      if (!element.closest('.cms-editable')) {
+        element.addEventListener('click', event => event.preventDefault());
+      }
+    });
+
+
+    // Prevent labels from assigning focus to inputs
+    [...doc.querySelectorAll('label[for]')].map(element => {
+      element.addEventListener('click', event => event.preventDefault());
+    });
+
+    // Prevent submit forms
+    [...doc.querySelectorAll('[type="submit"]')].map(element => {
+      element.addEventListener('click', event => event.preventDefault());
+    });
+
+    // Make inputs readonly
+    [...doc.querySelectorAll('input, textarea')].map(element => {
+      if (!element.closest('.cms-editable')) {
+        element.addEventListener('focus', event => event.preventDefault());
+      }
+    });
+
+    // Prevent form submissions
+    [...doc.querySelectorAll('form')].map(element => {
+      if (!element.closest('.cms-editable')) {
+        element.addEventListener('submit', event => event.preventDefault());
+      }
+    });
+  }
+
+  simulateClicks(iframe: HTMLIFrameElement, parentDoc: Document) {
+    const doc = iframe.contentWindow!.document;
+
+    doc.addEventListener('click', () => {
+      ['mousedown', 'mouseup', 'click'].map(eventType => {
+        const event = parentDoc.createEvent('MouseEvents');
+        event.initEvent(eventType, true, true);
+        parentDoc.body.dispatchEvent(event);
+      });
+    });
+  }
+
+
   get(url: string): Observable<any> {
     return new Observable((observer: Subscriber<any>) => {
       let objectUrl: string = "";
